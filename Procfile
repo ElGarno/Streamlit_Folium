00a5aa6da2427c38bafd30a093e44e20a6426fd0
elgarno <faffi@gmx.de>
2021-12-26 21:18:37 +0100

    changed deployment and deleted docker

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh create_config.sh && streamlit run streamlit_folium_companies.py
--- startup.sh
+++
@@ -1,2 +0,0 @@
-echo PORT $PORT
-streamlit run --server.enableCORS false --server.port $PORT streamlit_folium_companies.py
