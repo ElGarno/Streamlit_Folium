1a50f075908efc21d9cab231ca9e786b28aec5d1
elgarno <faffi@gmx.de>
2021-12-26 20:36:20 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import re
+import pandas as pd
+
+
+def show_map():
+    """
+    test
+    :return:
+    """
+    st.header("Test")
+
+
+if __name__ == "__main__":
+    show_map()
